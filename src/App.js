f119b3b7ef252d5b849c25341c3c0da88a32612c
Andjelina <astojanovic1900@gmail.com>
2022-05-12 10:31:37 +0200

    Add commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,36 @@ import { useState } from 'react';
 function App() {
   const [color, setColor] = useState(randomColor.randomColor());
 
-  // const color = randomColor();
-
   return (
-    <div
-      style={{
-        height: '80vh',
-        width: '80vw',
-        backgroundColor: color,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <button onClick={() => setColor(randomColor.randomColor())}>
-        Generate
-      </button>
+    <div>
+      <h1 style={{ color: color, textAlign: 'center', fontSize: '3rem' }}>
+        React Random Color Generator
+      </h1>
+      <div
+        style={{
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: color,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <button
+            className="btn"
+            onClick={() => setColor(randomColor.randomColor())}
+          >
+            Generate
+          </button>
+          <br />
+          <br />
+          <div style={{ marginTop: '30px', fontSize: '20px' }}>
+            Generated Color : <button className="col">{color}</button> Here we
+            have color
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
